8d764ca7cd5bc49fc54c11a673357a16b1fdbfdd
avalojandro <avalojandro@gmail.com>
2023-07-20 19:55:32 -0600

    :sparkles: Lisen App

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,12 +1,54 @@
-import './ExploreContainer.css';
+import {
+  IonFab,
+  IonIcon,
+  IonFabButton,
+  IonButton,
+  IonChip,
+  IonLabel,
+  IonAlert,
+} from "@ionic/react";
+import "./ExploreContainer.css";
+import { add, micOutline, mic, close } from "ionicons/icons";
+import { useState } from "react";
 
-interface ContainerProps { }
+interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    <div>
+      <div id="container">
+        <IonFabButton>
+          <IonIcon icon={mic} id="micIcon"></IonIcon>
+        </IonFabButton>
+      </div>
+      <div className="center">
+        <h1 className="title">Keywords:</h1>
+        <div className="keywords-container">
+          <IonChip id="present-alert">
+            <IonLabel onClick={() => setIsOpen(true)}>Alejandro</IonLabel>
+            <IonIcon icon={close}></IonIcon>
+          </IonChip>
+          <IonAlert
+            isOpen={isOpen}
+            trigger="present-alert"
+            header="Edit keyword:"
+            subHeader="Alejandro"
+            inputs={[
+              {
+                placeholder: "Alejandro",
+              },
+            ]}
+            buttons={["OK"]}
+          ></IonAlert>
+        </div>
+        <br />
+        <IonButton>
+          <IonIcon icon={add}></IonIcon>
+          &nbsp;Add
+        </IonButton>
+      </div>
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Lisen App</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
